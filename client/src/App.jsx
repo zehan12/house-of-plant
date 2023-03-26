c39928e6191374744f96314e191c627e4cb880d8
Zehan Khan <zehan9211@gmail.com>
2023-03-27 01:19:11 +0530

    header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
+import { Fragment } from 'react'
 import './App.css'
+import Header from './components/layouts/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='text-red-700 text-3xl font-bold'>House of Plant</h1>
-    </div>
+  return (<Fragment>
+    <Header />
+  </Fragment>
+     
   )
 }
 
